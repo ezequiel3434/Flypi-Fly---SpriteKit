cb5052ae85b876102ac8bfe5c2e9aef53f421cfd
ezequiel3434 <ezequiel3434@gmail.com>
2020-08-02 16:39:41 -0300

    adding a node, textures and animation

--- Flypi Fly - SpriteKit/GameScene.swift
+++ Flypi Fly - SpriteKit/GameScene.swift
@@ -12,14 +12,29 @@ import GameplayKit
 class GameScene: SKScene {
     
     
+    var fly = SKSpriteNode()
+    
     
     override func didMove(to view: SKView) {
         
-        // Get label node from scene and store it for use later
+        // Textures
+        let flyTexture1 = SKTexture(imageNamed: "fly1.png")
+        let flyTexture2 = SKTexture(imageNamed: "fly2.png")
         
-       
+        //Actions
         
-        // Create shape node to use during mouse interaction
+        let animation = SKAction.animate(with: [flyTexture1, flyTexture2], timePerFrame: 0.1)
+        let infiniteAnimation = SKAction.repeatForever(animation)
+        
+       fly = SKSpriteNode(texture: flyTexture1)
+        
+        fly.position = CGPoint(x: 0.0, y: 0.0)
+        
+        fly.run(infiniteAnimation)
+        
+        self.addChild(fly)
+        
+        
        
     }
     
