2939ec9522a3d6839594699fc375b10c4a9f556c
ezequiel3434 <ezequiel3434@gmail.com>
2020-08-02 18:17:29 -0300

    adding background image

--- Flypi Fly - SpriteKit/GameScene.swift
+++ Flypi Fly - SpriteKit/GameScene.swift
@@ -13,27 +13,39 @@ class GameScene: SKScene {
     
     
     var fly = SKSpriteNode()
-    
+    var BGImage = SKSpriteNode()
     
     override func didMove(to view: SKView) {
         
-        // Textures
+        
+        //Background
+               
+               let texturaFondo = SKTexture(imageNamed: "fondo.png")
+               BGImage = SKSpriteNode(texture: texturaFondo)
+               
+               BGImage.size.height = self.frame.height
+                
+        BGImage.zPosition = -1
+               self.addChild(BGImage)
+        
+        // Fly
         let flyTexture1 = SKTexture(imageNamed: "fly1.png")
         let flyTexture2 = SKTexture(imageNamed: "fly2.png")
         
         //Actions
         
-        let animation = SKAction.animate(with: [flyTexture1, flyTexture2], timePerFrame: 0.1)
+        let animation = SKAction.animate(with: [flyTexture1, flyTexture2], timePerFrame: 0.08)
         let infiniteAnimation = SKAction.repeatForever(animation)
         
        fly = SKSpriteNode(texture: flyTexture1)
         
-        fly.position = CGPoint(x: 0.0, y: 0.0)
+        fly.position = CGPoint(x: self.frame.midX, y: self.frame.midY)
         
         fly.run(infiniteAnimation)
         
         self.addChild(fly)
         
+       
         
        
     }
